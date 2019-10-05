e4a751db4d139b70663b073f5735c3001569523a
“”H <1573572086@qq.com>
2019-10-05 16:43:33 +0800

    创建视图login

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from diango.shortcuts import redirect
 
 def index(request):
-    return HttpResponse('index)
+    return HttpResponse('index')
 
 def login(request):
     return redirect('/index')
